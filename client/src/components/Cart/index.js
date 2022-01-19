f97b09dc2027aa87339c7d651c6ad7817ffba2cf
unknown <ghaliarose89@gmail.com>
2022-01-19 16:21:03 -0600

    DoNe

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -6,75 +6,98 @@ import { useStoreContext } from '../../utils/GlobalState';
 import React, { useEffect } from "react";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
-
-
-
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/client';
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 const Cart = () => {
-    const [state, dispatch] = useStoreContext();
-
-useEffect(() => {
-  async function getCart() {
-    const cart = await idbPromise('cart', 'get');
-    dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
-  };
-
-  if (!state.cart.length) {
-    getCart();
-  }
-}, [state.cart.length, dispatch]);
-
-
-    function toggleCart() {
-        dispatch({ type: TOGGLE_CART });
+  const [state, dispatch] = useStoreContext();
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+  useEffect(() => {
+    async function getCart() {
+      const cart = await idbPromise('cart', 'get');
+      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
     };
-    function calculateTotal() {
-        let sum = 0;
-        state.cart.forEach(item => {
-          sum += item.price * item.purchaseQuantity;
-        });
-        return sum.toFixed(2);
-      }
 
-    if (!state.cartOpen) {
-        return (
-            <div className="cart-closed" onClick={toggleCart}>
-                <span
-                    role="img"
-                    aria-label="trash">🛒</span>
-            </div>
-        );
+    if (!state.cart.length) {
+      getCart();
     }
+  }, [state.cart.length, dispatch]);
+
+
+  // to get strip session Id
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
+
+  function toggleCart() {
+    dispatch({ type: TOGGLE_CART });
+  };
+  function calculateTotal() {
+    let sum = 0;
+    state.cart.forEach(item => {
+      sum += item.price * item.purchaseQuantity;
+    });
+    return sum.toFixed(2);
+  }
+
+  if (!state.cartOpen) {
     return (
-        <div className="cart">
-  <div className="close" onClick={toggleCart}>[close]</div>
-  <h2>Shopping Cart</h2>
-  {state.cart.length ? (
-    <div>
-      {state.cart.map(item => (
-        <CartItem key={item._id} item={item} />
-      ))}
-      <div className="flex-row space-between">
-        <strong>Total: ${calculateTotal()}</strong>
-        {
-          Auth.loggedIn() ?
-            <button>
-              Checkout
-            </button>
-            :
-            <span>(log in to check out)</span>
-        }
+      <div className="cart-closed" onClick={toggleCart}>
+        <span
+          role="img"
+          aria-label="trash">🛒</span>
       </div>
-    </div>
-  ) : (
-    <h3>
-      <span role="img" aria-label="shocked">
-        😱
-      </span>
-      You haven't added anything to your cart yet!
-    </h3>
-  )}
-</div>
     );
+
+  };
+  function submitCheckout() {
+    const productIds = [];
+
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+    getCheckout({
+      variables: { products: productIds }
+    });
+  }
+  return (
+    <div className="cart">
+      <div className="close" onClick={toggleCart}>[close]</div>
+      <h2>Shopping Cart</h2>
+      {state.cart.length ? (
+        <div>
+          {state.cart.map(item => (
+            <CartItem key={item._id} item={item} />
+          ))}
+          <div className="flex-row space-between">
+            <strong>Total: ${calculateTotal()}</strong>
+            {
+              Auth.loggedIn() ?
+                <button onClick={submitCheckout}>
+                  Checkout
+                </button>
+                :
+                <span>(log in to check out)</span>
+            }
+          </div>
+        </div>
+      ) : (
+        <h3>
+          <span role="img" aria-label="shocked">
+            😱
+          </span>
+          You haven't added anything to your cart yet!
+        </h3>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,10 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
-
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 const resolvers = {
+
+
   Query: {
     categories: async () => {
       return await Category.find();
@@ -50,8 +52,46 @@ const resolvers = {
       }
 
       throw new AuthenticationError('Not logged in');
-    }
+    },
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      const order = new Order({ products: args.products });
+      const { products } = await order.populate('products').execPopulate();
+      const line_items = [];
+
+      for (let i = 0; i < products.length; i++) {
+        // generate product id
+        const product = await stripe.products.create({
+          name: products[i].name,
+          description: products[i].description,
+          images: [`${url}/images/${products[i].image}`]
+        });
+
+        // generate price id using the product id
+        const price = await stripe.prices.create({
+          product: product.id,
+          unit_amount: products[i].price * 100,
+          currency: 'usd',
+        });
+
+        // add price id to the line items array
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        })
+      }
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url:  `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}`
+      });
+
+      return { session: session.id };
+    },
   },
+
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
@@ -83,6 +123,7 @@ const resolvers = {
 
       return await Product.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true });
     },
+
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
@@ -99,8 +140,12 @@ const resolvers = {
       const token = signToken(user);
 
       return { token, user };
-    }
+    },
+
+
+
   }
+
 };
 
 module.exports = resolvers;
